ff857a18bc26f0f306b5fb613a1d28175037aa4e
Daniel Cárdenas <daniellogic@gmail.com>
2014-07-06 00:55:54 -0500

    Migrado correctamente a posgresql, necesario para utilizar heroku.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140621210837) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "merchants", force: true do |t|
     t.string   "user"
     t.string   "pass"
